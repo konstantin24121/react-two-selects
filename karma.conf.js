2e504c220b6d0127623c7369ffe2a0c36b111388
Konstantin <konstantin24121@gmail.com>
2016-10-11 11:30:17 +0500

    ui test
    парочка первых тестов

--- karma.conf.js
+++ karma.conf.js
@@ -31,10 +31,9 @@ module.exports = function(config) {
 			module: {
 				loaders: [
 					{ test: /\.(jpe?g|png|gif|svg)$/, loader: 'url', query: { limit: 10240 } },
-					{ test: /\.js$/, exclude: /node_modules/, loaders: ['babel'] },
+					{ test: /\.jsx?$/, exclude: /node_modules/, loaders: ['babel'] },
 					{ test: /\.json$/, loader: 'json-loader' },
-					{ test: /\.less$/, loader: 'style!css!less' },
-					{ test: /\.scss$/, loader: 'style!css?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]!autoprefixer?browsers=last 2 version!sass?outputStyle=expanded&sourceMap' }
+					{ test: /\.scss$/, loader: 'style!css!sass' }
 				]
 			},
 			resolve: {
@@ -42,11 +41,16 @@ module.exports = function(config) {
 					'src',
 					'node_modules'
 				],
-				extensions: ['', '.json', '.js']
+				alias: {
+					'@nm': 'node_modules',
+					'@app': 'src',
+					'@static': 'static'
+				},
+				extensions: ['', '.json', '.js', '.jsx', '.css', '.scss']
 			},
 			plugins: [
 				new webpack.IgnorePlugin(/\.json$/),
-				new webpack.NoErrorsPlugin(),
+				new webpack.NoErrorsPlugin()
 			]
 		},
 
--- test/Dropdown-spec.js
+++ test/Dropdown-spec.js
@@ -1,9 +1,43 @@
-import chai from 'chai';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {renderIntoDocument, Simulate} from 'react-addons-test-utils';
+import {expect} from 'chai';
+import Dropdown from '../src/components/Dropdown';
 
-const expect = chai.expect;
+describe('Dropdown conponent', () => {
 
-describe('InfoBar', () => {
-	it('shoud return 1', () => {
-		return expect(1).to.equal(1);
-	})
+	const renderer = renderIntoDocument(<Dropdown />)
+	const dom = ReactDOM.findDOMNode(renderer);
+
+	it('should render correctly', () => {
+		return expect(renderer).to.be.ok;
+	});
+
+	it('should render with correct default values', () => {
+		const defaultLabel = 'dropdown toogle';
+		const defaultToggleTag = 'span';
+
+		const label = dom.getElementsByClassName('dropdown__toggle')[0].textContent;
+		const tag = dom.getElementsByClassName('dropdown__toggle')[0].tagName.toLowerCase();
+
+		expect(label).to.equal(defaultLabel);
+		expect(tag).to.equal(defaultToggleTag);
+	});
+
+	// it('should render with warning if wrong type', () => {
+	// 	const type = 'table';
+	// 	const renderer = renderIntoDocument.bind(null, <Dropdown type = {type}/>)
+	// 	expect(renderer).to.throw(Error);
+	// });
+
+	it('should set correct state after selected', () => {
+		const trueLabel = 'Mars';
+		const items = [{id:1, label: trueLabel}, {id:2, label: 'Venus'}];
+
+		const renderer = renderIntoDocument(<Dropdown list={items}/>)
+		const dom = ReactDOM.findDOMNode(renderer);
+		
+		Simulate.click(dom.getElementsByClassName('dropdown__item')[0]);
+		expect(renderer.state.selectedItem).to.equal(1);
+	});
 })
